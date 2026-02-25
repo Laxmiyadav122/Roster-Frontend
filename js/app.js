a9b0c5765401b3eeb2b2d36ba643ddd26011a41a
Laxmiyadav122 <laxmi23@navgurukul.org>
2026-02-25 10:02:41 +0530

    Update frontend  with new changes

--- js/app.js
+++ js/app.js
@@ -4,3 +4,14 @@ const navMenu = document.getElementById("navMenu");
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
+
+const slides = document.querySelectorAll('#heroSlider img');
+let currentSlide = 0;
+
+function showNextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(showNextSlide, 3000);
